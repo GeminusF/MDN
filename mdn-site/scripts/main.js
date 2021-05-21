1379b51d5ca104a133ce15e0e1d74d4e0b3cc87f
GeminusF <78738210+GeminusF@users.noreply.github.com>
2021-05-22 01:58:06 +0400

    Commit

---
+++ mdn-site/scripts/main.js
@@ -0,0 +1,13 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello World';
+
+let myVariable = 'Frey';
+
+let iceCream = "ciyelek";
+
+if (iceCream === "ciyelek"){
+  alert('Yay, I love ciyelek ice cream')
+}
+else {
+  alert('Awwww, but  ciyelek ice cream is my favorite')
+}
