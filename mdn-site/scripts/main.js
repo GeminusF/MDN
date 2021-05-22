5c39dcfe2a4a4122d70dbe5a85c9fec7b5fe305f
GeminusF <78738210+GeminusF@users.noreply.github.com>
2021-05-22 21:40:19 +0400

    JS Event

--- mdn-site/scripts/main.js
+++ mdn-site/scripts/main.js
@@ -6,8 +6,30 @@ let myVariable = 'Frey';
 let iceCream = "ciyelek";
 
 if (iceCream === "ciyelek"){
-  alert('Yay, I love ciyelek ice cream')
+  alert('Welcom to site')
 }
 else {
   alert('Awwww, but  ciyelek ice cream is my favorite')
+}
+
+/*document.querySelector('html').onclick = function() {
+    alert('I Won’t Kill You, Because You’re Just Too Much Fun.');
+}*/
+
+
+document.querySelector('h3').onclick = function changeContent() {
+      document.querySelector('h3').textContent = ' Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine, Maggie Gyllenhaal, Gary Oldman';
+      document.querySelector('h3').style = 'Color:blue'
+}
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+      let mySrc = myImage.getAttribute('src');
+      if (mySrc === 'images/joker.png') {
+        myImage.setAttribute('src', 'images/jokerInPoliceCar.png');
+      }
+      else {
+        myImage.setAttribute('src', 'images/joker.png');
+      }
 }
